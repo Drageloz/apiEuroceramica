d6938e208f1187552792df994266564dcc90c0b0
drageloz <drageloz@gmail.com>
2021-01-24 17:44:53 -0500

    agregado inicio de llamado a api

--- src/main/java/com/euroceramica/dashboard/webController/UsersController.java
+++ src/main/java/com/euroceramica/dashboard/webController/UsersController.java
@@ -3,6 +3,8 @@ package com.euroceramica.dashboard.webController;
 import com.euroceramica.dashboard.domain.Users;
 import com.euroceramica.dashboard.domain.service.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +19,8 @@ public class UsersController {
     private UsersService usersService;
 
     @GetMapping("/all")
-    public List<Users> getAll(){
-        return usersService.getAll();
+    public ResponseEntity<List<Users>> getAll(){
+
+        return new ResponseEntity<>(usersService.getAll(), HttpStatus.OK);
     }
 }
---
+++ src/main/java/com/euroceramica/dashboard/webController/startController.java
@@ -0,0 +1,18 @@
+package com.euroceramica.dashboard.webController;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("")
+public class startController {
+
+    @GetMapping("")
+    public ResponseEntity<Boolean> startConection(){
+        return new ResponseEntity(true, HttpStatus.OK);
+    }
+}
