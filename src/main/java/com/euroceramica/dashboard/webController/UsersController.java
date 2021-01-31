c24dbe6df6c8071318792677880edc3c02dfb6eb
drageloz <drageloz@gmail.com>
2021-01-31 10:43:54 -0500

    allowed cros

--- src/main/java/com/euroceramica/dashboard/webController/UsersController.java
+++ src/main/java/com/euroceramica/dashboard/webController/UsersController.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/Users")
+@CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST}
 public class UsersController {
 
     @Autowired
