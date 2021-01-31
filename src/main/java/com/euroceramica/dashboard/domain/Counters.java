87cc0df1416b015046266d7798820acdfa55431a
drageloz <drageloz@gmail.com>
2021-01-31 14:47:26 -0500

    corregida clase Counters para mapeo

--- src/main/java/com/euroceramica/dashboard/domain/Counters.java
+++ src/main/java/com/euroceramica/dashboard/domain/Counters.java
@@ -2,49 +2,49 @@ package com.euroceramica.dashboard.domain;
 
 public class Counters {
 
-    private Integer co_id;
-    private String co_name_counter;
-    private String co_short_name;
-    private String co_process_name;
-    private Integer co_limit;
+    private Integer coId;
+    private String nameCounter;
+    private String shortName;
+    private String processName;
+    private Integer limit;
 
-    public Integer getCo_id() {
-        return co_id;
+    public Integer getCoId() {
+        return coId;
     }
 
-    public void setCo_id(Integer co_id) {
-        this.co_id = co_id;
+    public void setCoId(Integer coId) {
+        this.coId = coId;
     }
 
-    public String getCo_name_counter() {
-        return co_name_counter;
+    public String getNameCounter() {
+        return nameCounter;
     }
 
-    public void setCo_name_counter(String co_name_counter) {
-        this.co_name_counter = co_name_counter;
+    public void setNameCounter(String nameCounter) {
+        this.nameCounter = nameCounter;
     }
 
-    public String getCo_short_name() {
-        return co_short_name;
+    public String getShortName() {
+        return shortName;
     }
 
-    public void setCo_short_name(String co_short_name) {
-        this.co_short_name = co_short_name;
+    public void setShortName(String shortName) {
+        this.shortName = shortName;
     }
 
-    public String getCo_process_name() {
-        return co_process_name;
+    public String getProcessName() {
+        return processName;
     }
 
-    public void setCo_process_name(String co_process_name) {
-        this.co_process_name = co_process_name;
+    public void setProcessName(String processName) {
+        this.processName = processName;
     }
 
-    public Integer getCo_limit() {
-        return co_limit;
+    public Integer getLimit() {
+        return limit;
     }
 
-    public void setCo_limit(Integer co_limit) {
-        this.co_limit = co_limit;
+    public void setLimit(Integer limit) {
+        this.limit = limit;
     }
 }
