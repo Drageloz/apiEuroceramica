3264e8639c5160db44e03fdf725e26e09e586236
drageloz <drageloz@gmail.com>
2021-01-31 10:48:50 -0500

    allowed cros

--- src/main/java/com/euroceramica/dashboard/webController/UsersController.java
+++ src/main/java/com/euroceramica/dashboard/webController/UsersController.java
@@ -5,16 +5,18 @@ import com.euroceramica.dashboard.domain.service.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/Users")
-@CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST}
+@CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST})
 public class UsersController {
 
     @Autowired
