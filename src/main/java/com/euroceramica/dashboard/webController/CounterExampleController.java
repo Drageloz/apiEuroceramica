63ee4075cd346e47b49e57ff404d3a7af629a4b3
drageloz <drageloz@gmail.com>
2021-03-16 12:52:12 -0500

    added post and requestbody to set value

--- src/main/java/com/euroceramica/dashboard/webController/CounterExampleController.java
+++ src/main/java/com/euroceramica/dashboard/webController/CounterExampleController.java
@@ -2,12 +2,15 @@ package com.euroceramica.dashboard.webController;
 
 import com.euroceramica.dashboard.domain.CounterExample;
 import com.euroceramica.dashboard.domain.service.CounterExampleService;
+import com.euroceramica.dashboard.webController.requestBodyMapping.Value;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -43,9 +46,16 @@ public class CounterExampleController {
     }
 
     @GetMapping("/set/{value}")
-    public ResponseEntity<CounterExample> setValue(@PathVariable("value") Integer value){
+    public ResponseEntity<CounterExample> set(@PathVariable("value") Integer value){
         CounterExample counterExample = counterExampleService.getCounterById(1).get();
         counterExample.setCeValue(value);
+        return new ResponseEntity<CounterExample>(counterExampleService.save(counterExample), HttpStatus.OK);
+    }
+
+    @PostMapping(path = "/setValue")
+    public ResponseEntity<CounterExample> setValue(@RequestBody Value value){
+        CounterExample counterExample = counterExampleService.getCounterById(1).get();
+        counterExample.setCeValue(Integer.valueOf(value.getValue()));
         return new ResponseEntity<CounterExample>(counterExampleService.save(counterExample), HttpStatus.OK);
     }
 
---
+++ src/main/java/com/euroceramica/dashboard/webController/requestBodyMapping/Value.java
@@ -0,0 +1,13 @@
+package com.euroceramica.dashboard.webController.requestBodyMapping;
+
+public class Value {
+    private String value;
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
